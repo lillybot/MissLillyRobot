dcdfee0f8885a90e1676f8ea30aaa894e85fecb4
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-10-09 10:16:14 +0530

    Update chatbot.py

--- alexa/modules/chatbot.py
+++ alexa/modules/chatbot.py
@@ -659,91 +659,77 @@
 #     if any, to sign a "copyright disclaimer" for the program, if necessary.
 #     For more information on this, and how to apply and follow the GNU AGPL, see
 #     <https://www.gnu.org/licenses/>.
-import html
 # AI module using Intellivoid's Coffeehouse API by @TheRealPhoenix
-from time import sleep, time
+from time import time, sleep
 
-import alexa.modules.sql.chatbot_sql as sql
+from coffeehouse.lydia import LydiaAI
 from coffeehouse.api import API
 from coffeehouse.exception import CoffeeHouseError as CFError
-from coffeehouse.lydia import LydiaAI
-from alexa import LYDIA_API_KEY, OWNER_ID, dispatcher
-from alexa.modules.helper_funcs.chat_status import user_admin, user_can_change
+
+from telegram import Update, Bot
+from telegram.ext import CommandHandler, MessageHandler, Filters, run_async
+from telegram.error import BadRequest, Unauthorized, RetryAfter
+
+from alexa import dispatcher, LYDIA_API_KEY, OWNER_ID
+import alexa.modules.sql.chatbot_sql as sql
+from alexa.modules.helper_funcs.chat_status import user_admin
 from alexa.modules.helper_funcs.filters import CustomFilters
-from alexa.modules.log_channel import loggable
-from telegram import Update
-from telegram.error import BadRequest, RetryAfter, Unauthorized
-from telegram.ext import (CallbackContext, CommandHandler, Filters,
-                          MessageHandler, run_async)
-from telegram.utils.helpers import mention_html
 
 CoffeeHouseAPI = API(LYDIA_API_KEY)
 api_client = LydiaAI(CoffeeHouseAPI)
 
 
 @run_async
-#@user_can_change
-@loggable
-def add_chat(update: Update, context: CallbackContext):
+@user_admin
+def add_chat(_bot: Bot, update: Update):
     global api_client
-    chat = update.effective_chat
+    chat_id = update.effective_chat.id
     msg = update.effective_message
-    user = update.effective_user
-    is_chat = sql.is_chat(chat.id)
+    is_chat = sql.is_chat(chat_id)
     if not is_chat:
         ses = api_client.create_session()
         ses_id = str(ses.id)
         expires = str(ses.expires)
-        sql.set_ses(chat.id, ses_id, expires)
+        sql.set_ses(chat_id, ses_id, expires)
         msg.reply_text("AI successfully enabled for this chat!")
-        message = (f"<b>{html.escape(chat.title)}:</b>\n"
-                   f"#AI_ENABLED\n"
-                   f"<b>Admin:</b> {mention_html(user.id, user.first_name)}\n")
-        return message
     else:
         msg.reply_text("AI is already enabled for this chat!")
-        return ""
+
 
 @run_async
-#@user_can_change
-@loggable
-def remove_chat(update: Update, context: CallbackContext):
+@user_admin
+def remove_chat(_bot: Bot, update: Update):
     msg = update.effective_message
-    chat = update.effective_chat
-    user = update.effective_user
-    is_chat = sql.is_chat(chat.id)
+    chat_id = update.effective_chat.id
+    is_chat = sql.is_chat(chat_id)
     if not is_chat:
         msg.reply_text("AI isn't enabled here in the first place!")
-        return ""
     else:
-        sql.rem_chat(chat.id)
+        sql.rem_chat(chat_id)
         msg.reply_text("AI disabled successfully!")
-        message = (f"<b>{html.escape(chat.title)}:</b>\n"
-                   f"#AI_DISABLED\n"
-                   f"<b>Admin:</b> {mention_html(user.id, user.first_name)}\n")
-        return message
 
-def check_message(context: CallbackContext, message):
+
+def check_message(bot: Bot, message):
     reply_msg = message.reply_to_message
     if message.text.lower() == "alexa":
         return True
     if reply_msg:
-        if reply_msg.from_user.id == context.bot.get_me().id:
+        if reply_msg.from_user.id == bot.get_me().id:
             return True
     else:
         return False
 
+
 @run_async
-def chatbot(update: Update, context: CallbackContext):
+def chatbot(bot: Bot, update: Update):
     global api_client
     msg = update.effective_message
     chat_id = update.effective_chat.id
     is_chat = sql.is_chat(chat_id)
-    bot = context.bot
     if not is_chat:
         return
     if msg.text and not msg.document:
-        if not check_message(context, msg):
+        if not check_message(bot, msg):
             return
         sesh, exp = sql.get_ses(chat_id)
         query = msg.text
@@ -762,16 +748,17 @@ def chatbot(update: Update, context: CallbackContext):
             sleep(0.3)
             msg.reply_text(rep, timeout=60)
         except CFError as e:
-            bot.send_message(OWNER_ID,
-                             f"Chatbot error: {e} occurred in {chat_id}!")
+            bot.send_message(
+                OWNER_ID, f"Chatbot error: {e} occurred in {chat_id}!")
+
 
 @run_async
-def list_chatbot_chats(update: Update, context: CallbackContext):
+def list_chatbot(bot: Bot, update: Update):
     chats = sql.get_all_chats()
     text = "<b>AI-Enabled Chats</b>\n"
     for chat in chats:
         try:
-            x = context.bot.get_chat(int(*chat))
+            x = bot.get_chat(int(*chat))
             name = x.title if x.title else x.first_name
             text += f"• <code>{name}</code>\n"
         except BadRequest:
@@ -783,8 +770,12 @@ def list_chatbot_chats(update: Update, context: CallbackContext):
     update.effective_message.reply_text(text, parse_mode="HTML")
 
 
+__mod_name__ = "Chatbot"
+
 ADD_CHAT_HANDLER = CommandHandler("addchat", add_chat)
 REMOVE_CHAT_HANDLER = CommandHandler("rmchat", remove_chat)
+
+# Filters for ignoring #note messages, !commands and sed.
 
 dispatcher.add_handler(ADD_CHAT_HANDLER)
 dispatcher.add_handler(REMOVE_CHAT_HANDLER)
