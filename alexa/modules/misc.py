b320ad3d1eb567d64a80c3f3ccb4ce1c01ccd09c
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-10-11 09:39:53 +0530

    Update misc.py

--- alexa/modules/misc.py
+++ alexa/modules/misc.py
@@ -2550,19 +2550,19 @@ from telethon.tl.functions.channels import (EditAdminRequest,
 
 def online_within(participant, days):
     status = participant.status
-    if isinstance(status, t.UserStatusOnline):
+    if isinstance(status, UserStatusOnline):
         return True
 
-    last_seen = status.was_online if isinstance(status, t.UserStatusOffline) else None
+    last_seen = status.was_online if isinstance(status, UserStatusOffline) else None
 
     if last_seen:
         now = datetime.datetime.now(tz=datetime.timezone.utc)
         diff = now - last_seen
         return diff <= datetime.timedelta(days=days)
 
-    if isinstance(status, t.UserStatusRecently) and days >= 1 \
-            or isinstance(status, t.UserStatusLastWeek) and days >= 7 \
-            or isinstance(status, t.UserStatusLastMonth) and days >= 30:
+    if isinstance(status, UserStatusRecently) and days >= 1 \
+            or isinstance(status, UserStatusLastWeek) and days >= 7 \
+            or isinstance(status, UserStatusLastMonth) and days >= 30:
         return True
 
     return False
