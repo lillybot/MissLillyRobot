e1142dba3681efb26a6795f705ba84b07a0d3964
MissAlexaRobot <54566209+MissAlexaRobot@users.noreply.github.com>
2020-10-12 19:23:08 +0530

    Update games.py

--- alexa/modules/games.py
+++ alexa/modules/games.py
@@ -3,10 +3,13 @@ __mod_name__ = "Games ⚽"
 __help__ = """
 *Here are some cool games for you enjoy them, more coming in the future !*
 
-/motofx
-/mathbattle
-/penaltyshooter
-/racingcar
-/karate
-/footballstar
+ - /motofx
+ - /mathbattle
+ - /penaltyshooter
+ - /racingcar
+ - /karate
+ - /footballstar
+ - /gravityninja 
+ - /discoball
+ - /neonblaster
 """
