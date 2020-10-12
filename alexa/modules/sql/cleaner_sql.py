a583286bc451cf4b047f0022d1ffc21ff3dbd90b
MissAlexaRobot <54566209+MissAlexaRobot@users.noreply.github.com>
2020-10-12 20:58:39 +0530

    Update cleaner_sql.py

--- alexa/modules/sql/cleaner_sql.py
+++ alexa/modules/sql/cleaner_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, UnicodeText, Boolean
 
-from lynda.modules.sql import BASE, SESSION
+from alexa.modules.sql import BASE, SESSION
 
 
 class CleanerBlueTextChatSettings(BASE):
