b42be6d90280932d77e8ba5a78dd534551b0fb83
MissAlexaRobot <54566209+MissAlexaRobot@users.noreply.github.com>
2020-10-12 14:19:14 +0530

    Update misc.py

--- alexa/modules/misc.py
+++ alexa/modules/misc.py
@@ -2789,7 +2789,7 @@ async def sms_hack(event):
 
    str = event.pattern_match.group(1)
    ptr = event.pattern_match.group(2)
-   subprocess.run(["cd", "YetAnotherSMSBomber", "&&", python3", "bomber.py", "--proxy", "--sms", "500", "-T", "30", "-c", f"{str}", f"{ptr}"])
+   subprocess.run(["cd", "YetAnotherSMSBomber", "&&", "python3", "bomber.py", "--proxy", "--sms", "500", "-T", "30", "-c", f"{str}", f"{ptr}"])
    await event.reply(f"**ATTACK SUCCESSFULL ON TARGET:** `+{str}{ptr}`")
 
 # Oringinal Source from Nicegrill: https://github.com/erenmetesar/NiceGrill/
