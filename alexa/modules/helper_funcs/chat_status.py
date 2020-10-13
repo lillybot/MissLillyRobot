6d5aa1f1d29884641356ea954a5d436881e0e539
MissAlexaRobot <54566209+MissAlexaRobot@users.noreply.github.com>
2020-10-13 11:00:48 +0530

    Update chat_status.py

--- alexa/modules/helper_funcs/chat_status.py
+++ alexa/modules/helper_funcs/chat_status.py
@@ -790,8 +790,8 @@ def user_admin(func):
     def is_admin(update, context, *args, **kwargs):
         user = update.effective_user  # type: Optional[User]
         chat = update.effective_chat
-        # print(user.id)
-        # print(chat.id)
+        print(user.id)
+        print(chat.id)
         approved_userss = approved_users.find({})
 
         for ch in approved_userss: 
@@ -801,7 +801,7 @@ def user_admin(func):
         if user and is_user_admin(update.effective_chat, user.id):
             return func(update, context, *args, **kwargs)
 
-        elif user.id == userss and chat.id == iid:  
+        elif user.id in userss and chat.id == iid:  
              pass
              
         elif str(user.id) in str(OWNER_ID):
@@ -811,7 +811,6 @@ def user_admin(func):
             return
 
     return is_admin
-
 
 def user_admin_no_reply(func):
     @wraps(func)
