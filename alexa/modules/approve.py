01a331d1c623632cd8aabfadf18f5e8d3db4f7a3
MissAlexaRobot <54566209+MissAlexaRobot@users.noreply.github.com>
2020-10-14 18:26:41 +0530

    Update approve.py

--- alexa/modules/approve.py
+++ alexa/modules/approve.py
@@ -806,41 +806,6 @@ async def disapprove(event):
 			return
 	await event.reply("This User isn't approved yet")
 	
-@register(pattern="^/checkstatus$")
-async def checkst(event):
-	if event.fwd_from:
-		return  
-	if MONGO_DB_URI is None:
-		return
-	chat_id = event.chat.id
-	sender = event.from_id 
-	reply_msg = await event.get_reply_message()	
-	approved_userss = approved_users.find({})
-	for ch in approved_userss: 
-		iid = ch['id']
-		userss = ch['user']
-	if event.is_group:
-		if not await can_approve_users(message=event):
-			return
-		else:
-			pass
-			
-	if await is_register_admin(event.input_chat, reply_msg.from_id):
-		await event.reply("Why will i check approve status of an admin ?")
-		return
-			
-	if not event.reply_to_msg_id:
-		await event.reply("Reply To Someone's Message To Check Status")
-		return	
-
-	chats = approved_users.find({})
-	for c in chats:
-		if event.chat_id == c['id'] and reply_msg.from_id == c['user']:
-			await event.reply("This User is Approved")
-			return 
-
-	await event.reply("This user isn't approved")
-
 
 @register(pattern="^/listapproved$")
 async def apprlst(event):
@@ -862,9 +827,8 @@ async def apprlst(event):
 	msg = "**APPROVED USERS**\n"
 	for i in autos:
 		if event.chat_id == i['id']:
+			dev = list(i['user'])
+			print(dev)
 			msg += " - "+str(i['user'])+"\n"
 	
 	await event.reply(msg)
-
-
-__mod_name__ = "Approval"
