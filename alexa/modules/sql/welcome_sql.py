5532ffa3e9041e5433e2d722d162037d00c2ca69
MissAlexaRobot <54566209+MissAlexaRobot@users.noreply.github.com>
2020-10-14 12:05:01 +0530

    Update welcome_sql.py

--- alexa/modules/sql/welcome_sql.py
+++ alexa/modules/sql/welcome_sql.py
@@ -1026,6 +1026,7 @@ def set_clean_service(chat_id: Union[int, str], setting: bool):
         SESSION.commit()
 
 
+
 def migrate_chat(old_chat_id, new_chat_id):
     with INSERTION_LOCK:
         chat = SESSION.query(Welcome).get(str(old_chat_id))
