90691adb86810c180929832e622bc0b39ee03426
MissAlexaRobot <54566209+MissAlexaRobot@users.noreply.github.com>
2020-10-15 17:50:29 +0530

    Update __init__.py

--- alexa/__init__.py
+++ alexa/__init__.py
@@ -834,12 +834,16 @@ if ENV:
     import pytz
     SQLDATEALERT = os.environ.get("SQLDATEALERT", None)
 
-    LT = datetime.datetime.now(pytz.timezone('Asia/Kolkata'))
-    OT = LT.strftime("%d:%m")
-    if str(OT) <= str(SQLDATEALERT):
-      while True: 
-         tbot.send_message(-1001158277850, "**ALERT**\n\n__Hello moderators please upgrade my SQL database for my proper functioning !\nSet a new DATABASE_URL__")
-         time.sleep(3600)
+    async def check_db():
+      LT = datetime.datetime.now(pytz.timezone('Asia/Kolkata'))
+      OT = LT.strftime("%d:%m")
+      if str(OT) <= str(SQLDATEALERT):
+        while True: 
+           await tbot.send_message(-1001158277850, "**ALERT**\n\n__Hello moderators please upgrade my SQL database for my proper functioning !\nSet a new DATABASE_URL__")
+           await asyncio.sleep(3600)
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(check_db())
+    loop.close()
 		
 else:
     quit(1)
