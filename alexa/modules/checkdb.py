4dffe02fcfb3df1f7de129ad644b144bf585a9fa
MissAlexaRobot <54566209+MissAlexaRobot@users.noreply.github.com>
2020-10-15 17:55:01 +0530

    Update checkdb.py

--- alexa/modules/checkdb.py
+++ alexa/modules/checkdb.py
@@ -10,7 +10,7 @@ async def check_db():
     if str(OT) <= str(SQLDATEALERT):
        while True: 
            await tbot.send_message(-1001158277850, "**ALERT**\n\n__Hello moderators please upgrade my SQL database for my proper functioning !\nSet a new DATABASE_URL__")
-           await asyncio.sleep(3600)
+           await asyncio.sleep(5)
     loop = asyncio.get_event_loop()
     loop.run_until_complete(check_db())
     loop.close()
