1848fd4ca6a650b0a1b827f173eaa0b9dd35ac80
MissAlexaRobot <54566209+MissAlexaRobot@users.noreply.github.com>
2020-10-15 18:54:12 +0530

    Update checkdb.py

--- alexa/modules/checkdb.py
+++ alexa/modules/checkdb.py
@@ -5,14 +5,12 @@ import pytz
 from alexa import tbot, SQLDATEALERT
 from telethon import events
 
-def decorator(func):        
-  async def check_db():
+async def check_db():
     LT = datetime.datetime.now(pytz.timezone('Asia/Kolkata'))
     OT = LT.strftime("%d|%m|%y")
     if str(OT) <= str(SQLDATEALERT):
-       while True: 
+       while True:
            await tbot.send_message(-1001158277850, "**ALERT**\n\n__Hello moderators please upgrade my SQL database for my proper functioning !\nSet a new DATABASE_URL__")
            await asyncio.sleep(5)
-    return check_db
-  return decorator
-    
+
+tbot.loop.run_until_complete(check_db())
