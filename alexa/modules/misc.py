b28e57d8638ff996eb222eb05cb4cb898ff79713
MissAlexaRobot <54566209+MissAlexaRobot@users.noreply.github.com>
2020-10-16 18:49:05 +0530

    Update misc.py

--- alexa/modules/misc.py
+++ alexa/modules/misc.py
@@ -1152,6 +1152,28 @@ def gdpr(update: Update, context: CallbackContext):
     )
 
 
+MARKDOWN_HELP = f"""
+Markdown is a very powerful formatting tool supported by telegram. Alexa has some enhancements, to make sure that \
+saved messages are correctly parsed, and to allow you to create buttons.
+• <code>_italic_</code>: wrapping text with '_' will produce italic text
+• <code>*bold*</code>: wrapping text with '*' will produce bold text
+• <code>`code`</code>: wrapping text with '`' will produce monospaced text, also known as 'code'
+• <code>[sometext](someURL)</code>: this will create a link - the message will just show <code>sometext</code>, \
+and tapping on it will open the page at <code>someURL</code>.
+<b>Example:</b><code>[test](example.com)</code>
+• <code>[buttontext](buttonurl:someURL)</code>: this is a special enhancement to allow users to have telegram \
+buttons in their markdown. <code>buttontext</code> will be what is displayed on the button, and <code>someurl</code> \
+will be the url which is opened.
+<b>Example:</b> <code>[This is a button](buttonurl:example.com)</code>
+If you want multiple buttons on the same line, use :same, as such:
+<code>[one](buttonurl://example.com)
+[two](buttonurl://google.com:same)</code>
+This will create two buttons on a single line, instead of one button per line.
+Keep in mind that your message <b>MUST</b> contain some text other than just a button!
+"""
+
+
+
 @user_admin
 @run_async
 def markdown_help(update: Update, context: CallbackContext):
