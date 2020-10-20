c5669146bebd3b2e17ae965c96334761c90fb722
Smoke <68583856+Smoke-007@users.noreply.github.com>
2020-10-20 20:47:20 +0530

    Update lydia.py

--- alexa/modules/lydia.py
+++ alexa/modules/lydia.py
@@ -663,7 +663,7 @@
 __help__ = """
 Powered by coffeehouse from `intellivoid.net`
 
- - /addchat: Activates AI mode in the chat the bot will give auto replies to anyone who tags the bot
- - /rmchat: Deactivates AI mode in the chat the bot will stop giving auto replies to anyone who tags the bot
+ - /addchat: Activates AI mode in the chat the bot will give auto replies to anyone who tags the bot😇
+ - /rmchat: Deactivates AI mode in the chat the bot will stop giving auto replies to anyone who tags the bot😕
 """
 __mod_name__ = "ChatBot 🤖"
